a65aa4027c948d623364ab9229a4fb314e73b32e
Federico <federicolucaspontoriero@gmail.com>
2022-04-25 18:49:31 -0300

    Styles for the footer

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 function NavigationDots({ active }) {
-  return (
-    <div className='app__navigation'>
-    {['home', 'about', 'contact', 'work', 'testimonials', 'contact', 'skills'].map((item, index) => ( 
-          <a key={item + index} className='app__navigation-dot' style={active === item ? { backgroundColor: '#313BAC' } : {}}
-            
-            href={`#${item}`} />
-       ))}
-    </div>
-  )
+	return (
+		<div className='app__navigation'>
+			{['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map(
+				(item, index) => (
+					<a
+						key={item + index}
+						className='app__navigation-dot'
+						style={active === item ? { backgroundColor: '#313BAC' } : {}}
+						href={`#${item}`}
+					/>
+				)
+			)}
+		</div>
+	);
 }
 
-export default NavigationDots
+export default NavigationDots;
