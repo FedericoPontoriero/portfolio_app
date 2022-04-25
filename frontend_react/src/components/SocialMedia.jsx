0b6a987aae7bb8784fd9d20991c0f858918a8ef5
Federico <federicolucaspontoriero@gmail.com>
2022-04-25 18:05:13 -0300

    Links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import {  BsLinkedin } from 'react-icons/bs'
-import { FiGithub } from "react-icons/fi";
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FiGithub } from 'react-icons/fi';
 
 function SocialMedia() {
-  return (
-    <div className='app__social'>
-        <div>
-            <FiGithub />
-        </div>
-        <div>
-            <BsLinkedin />
-        </div>
-    </div>
-  )
+	return (
+		<div className='app__social'>
+			<div>
+				<FiGithub href='https://github.com/FedericoPontoriero' />
+			</div>
+			<div>
+				<BsLinkedin href='https://www.linkedin.com/in/federico-pontoriero-924050219/' />
+			</div>
+		</div>
+	);
 }
 
-export default SocialMedia
+export default SocialMedia;
